dd4acc034aef42748331910c0d09b11145fd3088
rivu <58026024+unphydra@users.noreply.github.com>
2020-06-18 16:38:32 +0530

    modified cd to recognise tilder as home directory

--- my_shell.c
+++ my_shell.c
@@ -23,13 +23,21 @@ char ** get_args(char * command)
 void cd(char ** args)
 {
   char path[200];
-  if (strncmp(args[1],"/",1)!=0)
+  if (strncmp(args[1],"/",1)==0)
   {
-  getcwd(path,sizeof(path));
-  strcat(path,"/");
-  }  
-  strcat(path,args[1]);
-  chdir(path);
+    chdir(args[1]);
+  } else if(strncmp(args[1],"~",1)==0)
+  {
+    strcat(path,"/Users/step12");
+    strcat(path,&args[1][1]);
+    chdir(path);
+  } else
+  {
+    getcwd(path,sizeof(path));
+    strcat(path,"/");
+    strcat(path,args[1]);
+    chdir(path);
+  }
 }
 
 int main(void)
