5c0678022072fc90a6466794c7d14b017d10b2f4
rivu <58026024+unphydra@users.noreply.github.com>
2020-06-19 22:41:58 +0530

    added call_child function to fork and call exec in child

--- my_shell.c
+++ my_shell.c
@@ -7,6 +7,29 @@
 #include <signal.h>
 #include "colors.h"
 
+void call_child(char** args)
+{
+  int pid = fork();
+  if (pid == 0)
+  {
+    signal(SIGINT,SIG_DFL);
+    int result = execvp(args[0],args);
+    if (result < 0)
+    {
+      printf("my-shell: command not found: %s\n",args[0]);
+    }
+    
+    exit(0);
+  }
+  else
+  {
+    wait(NULL);
+   }
+  printf("\n");
+}
+
+
+
 char ** get_args(char * command)
 {
   int len = strlen(command);
@@ -59,22 +82,6 @@ int main(void)
       cd(args);
       continue;
     }
-    int pid = fork();
-    if (pid == 0)
-    {
-      signal(SIGINT,SIG_DFL);
-      int result = execvp(args[0],args);
-      if (result < 0)
-      {
-        printf("my-shell: command not found: %s\n",args[0]);
-      }
-      
-      exit(0);
-    }
-    else
-    {
-      wait(NULL);
-    }
-  printf("\n");
+    call_child(args);
   }
 }
